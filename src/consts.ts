19b3f98c41bce0c82d7b548c2cb931f564fe9028
callumma <callumma@motabilityoperations.co.uk>
2025-08-06 09:30:22 +0100

    New site

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "M24 Media";
+export const SITE_DESCRIPTION = "M24 Media builds tools for modern parenthood.";
